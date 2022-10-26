bbe242727f6082eccfebf1335af4fb8c35b833bf
charb29 <ocharb@mac.com>
2022-10-25 22:03:33 -0700

    Add shadow styles for button & text

--- src/screens/ForgotPasswordScreen/styles.js
+++ src/screens/ForgotPasswordScreen/styles.js
@@ -27,6 +27,20 @@ const styles = StyleSheet.create( {
         color: '#00BFFF',
         fontWeight: 'thin',
     },
+    textInputShadow: {
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 2,
+        elevation: 6,
+    },
+    buttonShadow: {
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 5,
+        elevation: 8,
+    },
     inputContainer: {
         backgroundColor: 'white',
         width: '90%',
