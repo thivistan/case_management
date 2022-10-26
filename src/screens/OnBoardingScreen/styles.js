cac89121006c06bc8dad2333d604f70e18c99b05
charb29 <ocharb@mac.com>
2022-10-25 20:26:28 -0700

    Configure shadows to button

--- src/screens/OnBoardingScreen/styles.js
+++ src/screens/OnBoardingScreen/styles.js
@@ -1,34 +1,60 @@
 import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+
+let deviceWidth = Dimensions.get('screen').width;
+let deviceHeight = Dimensions.get('screen').height;
 
 const styles = StyleSheet.create( {
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: 'white',
     },
-    textContainer: {
+    lightBlueSquare: {
+        position: 'absolute',
+        left: 0,
+        height: '65%',
+        width: '75%',
+        marginTop: '15%',
+        borderRadius: 40,
+        backgroundColor: '#89CFF0',
+    },
+    darkBlueSquare: {
+        position: 'absolute',
+        right: 0,
+        height: '65%',
+        width: '75%',
+        marginTop: '5%',
+        borderRadius: 40,
+        backgroundColor: '#00BFFF',
+    },
+    squareShadow: {
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 15,
+    },
+    footerContainer: {
+        width: deviceWidth,
         justifyContent: 'center',
         alignItems: 'center',
-        height: 275,
         position: 'absolute',
+        padding: '10%',
+        height: '35%',
         bottom: 0,
     },
-    welcomeText: {
+    footer: {
         color: '#00BFFF',
-        fontWeight: '500',
+        fontWeight: 'bold',
         fontSize: 16,
     },
-    infoText: {
-        color: '#00BFFF',
-        fontWeight: '500',
-    },
-    bottomButtonContainer: {
-        width: '100%',
-        height: 100,
+    buttonContainer: {
+        width: deviceWidth,
+        height: '15%',
         justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
-        bottom: 0
+        bottom: 0,
     },
     button: {
         backgroundColor: '#00BFFF',
@@ -39,27 +65,21 @@ const styles = StyleSheet.create( {
     },
     buttonText: {
         color: 'white',
-        fontWeight: '500',
+        fontWeight: 'bold',
         fontSize: 16,
     },
-    lightBlueSquare: {
-        position: 'absolute',
-        left: 0,
-        width: 350,
-        height: 400,
-        borderRadius: 40,
-        justifyContent: 'center',
-        backgroundColor: '#89CFF0',
+    buttonShadow: {
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 5,
+        elevation: 8,
     },
-    darkBlueSquare: {
-        position: 'absolute',
-        right: 0,
-        width: 350,
-        height: 400,
-        top: 60,
-        borderRadius: 40,
-        justifyContent: 'center',
-        backgroundColor: '#00BFFF',
+    shadow: {
+        shadowOffset: {width: -2, height: 4},
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     },
     thaddeusSquare: {
         position: 'absolute',
@@ -74,18 +94,15 @@ const styles = StyleSheet.create( {
         backgroundColor: '#FF1493',
     },
     thaddeusText: {
-        // Android font
         fontFamily: 'serif',
-        // Apple font
-        // fontFamily: 'Times New Roman',
         textTransform: 'uppercase',
         color: 'white',
         fontSize: 22,
         bottom: 25,
         fontWeight: 'bold',
         letterSpacing: 2,
-        marginLeft: 25,
-        marginRight: 25,
+        marginLeft: '10%',
+        marginRight: '10%',
     },
     resourceCenterText: {
         fontFamily: 'sans-serif',
@@ -95,28 +112,28 @@ const styles = StyleSheet.create( {
         fontWeight: 'bold',
         bottom: 25,
         letterSpacing: 3,
-        marginLeft: 25,
-        marginRight: 25,
+        marginLeft: '10%',
+        marginRight: '10%',
     },
     flexBox: {
         bottom: 25,
         flexDirection: 'row',
         alignItems: 'center',
-        marginLeft: 25,
-        marginRight: 25,
+        marginLeft: '10%',
+        marginRight: '10%',
     },
     lineStyle: {
         flex: 1,
         borderWidth: 0.5,
-        marginRight: 5,
-        marginLeft: 5,
+        marginRight: '3%',
+        marginLeft: '3%',
         borderColor: 'white',
     },
     year: {
         fontFamily: 'sans-serif',
         textAlign: 'center',
         color: 'white',
-        fontSize: 8,
+        fontSize: 10,
         alignSelf: 'center',
     }
 } )
