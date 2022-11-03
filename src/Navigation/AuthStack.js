7d1269f4e74828015902849b0cf46e0a816bb62b
charb29 <ocharb@mac.com>
2022-11-03 12:39:32 -0700

    Add screens related to auth

--- src/Navigation/AuthStack.js
+++ src/Navigation/AuthStack.js
@@ -3,20 +3,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "../screens/LoginScreen/LoginScreen";
 import OnBoardingScreen from "../screens/OnBoardingScreen/OnBoardingScreen";
 import ForgotPasswordScreen from "../screens/ForgotPasswordScreen/ForgotPasswordScreen";
-import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import RegistrationScreen from "../screens/RegistrationScreen/RegistrationScreen";
 
 const Stack = createNativeStackNavigator();
 
-const AuthStack = () => {
+export default function AuthStack() {
     return (
-        <Stack.Navigator screenOptions = {{ headerShown: false }}>
-            <Stack.Screen name = "OnBoarding" component = { OnBoardingScreen } />
-            <Stack.Screen name = "Login" component = { LoginScreen } />
-            <Stack.Screen name = "ForgotPassword" component = { ForgotPasswordScreen } />
-            <Stack.Screen name = "Registration" component = { RegistrationScreen } />
-        </Stack.Navigator>
+            <Stack.Navigator 
+                initialRouteName = "OnBoarding"
+                screenOptions = {{ 
+                    headerShown: false 
+                }}>
+                <Stack.Screen name = "OnBoarding" component = { OnBoardingScreen } />
+                <Stack.Screen name = "Login" component = { LoginScreen } />
+                <Stack.Screen name = "ForgotPassword" component = { ForgotPasswordScreen } />
+                <Stack.Screen name = "Registration" component = { RegistrationScreen } />
+            </Stack.Navigator>
       );
 }
-
-export default AuthStack;
