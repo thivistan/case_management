602368399d1fa39dcc937005a7dbbf768f394f07
charb29 <ocharb@mac.com>
2022-11-03 12:40:09 -0700

    Configure routes

---
+++ src/Navigation/Routes.js
@@ -0,0 +1,29 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { auth } from '../firebase/firebase';
+import AuthStack from './AuthStack';
+import MainStack from './MainStack';
+import { AuthContext } from './AuthProvider';
+
+export default function Routes() {
+    const { user, setUser } = useContext(AuthContext);
+    const [loading, setLoading] = useState(true);
+    const [initializing, setInitializing] = useState(true);
+
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+        setLoading(false);
+    }
+    
+    useEffect(() => {
+        const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
+        return subscriber;
+    }, []);
+    
+    return (
+        <NavigationContainer>
+            { user ? <MainStack /> : <AuthStack /> }
+        </NavigationContainer>
+    )
+}
