c36c7dcb4d16d4a69eac0c17af483d4f2c52ca02
charb29 <ocharb@mac.com>
2022-11-29 15:26:01 -0800

    Create custom font const

--- src/screens/MainScreens/HomeScreen/HomeScreen.js
+++ src/screens/MainScreens/HomeScreen/HomeScreen.js
@@ -1,10 +1,31 @@
 import React from "react";
+
+import { useFonts } from 'expo-font';
+import { SafeAreaView } from "react-native-safe-area-context";
+
 import { Text, View } from "react-native";
+import { Calendar } from "react-native-calendars";
 
 export default function HomeScreen() {
+    const [fontsLoaded] = useFonts({
+        'Montserrat-Black': require('../../../assets/fonts/Montserrat-Black.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null
+    };
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, paddingHorizontal: 25, backgroundColor: 'white', flexDirection: 'column' }}>
+            <View style={{ flex: 1, }}>
+                <Text
+                    style={{
+                        fontFamily: 'Montserrat-Black',
+                        fontSize: 16,
+                    }}>
+                    Upcoming appointments...</Text>
+
+            </View>
+        </SafeAreaView >
     )
 }
