b385e5f3397487f37cd6dd0989d59bb303f0c142
Nathan Goshay <naterg2000@gmail.com>
2023-04-04 15:31:21 -0700

    figured out realtime database connection
    firebase.js contains the realtime database export. HomeScreen.js contains an example on how to get data from the database! It currently prints the first inspirational quote to the console.

--- src/screens/MainScreens/HomeScreen/HomeScreen.js
+++ src/screens/MainScreens/HomeScreen/HomeScreen.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { Text, View } from "react-native";
-import ResourcePage from '../../../components/ResourcePage'
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import ResourcePage from '../../../components/ResourcePage';
+import { db } from '../../../firebase/firebase.js';
 export default function HomeScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
-    )
+  var starCountRef = db.ref('inspirational_quotes/1');
+  starCountRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
+  });
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Home Screen</Text>
+    </View>
+  );
 }
