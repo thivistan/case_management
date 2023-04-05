384167500694a697427c1d88626d81c5235738c3
Nathan Goshay <naterg2000@gmail.com>
2023-04-04 22:36:15 -0700

    Added random quote printing once per page load
    A quote is printed one time when the page is loaded. The quote won't be re-printed when you switch pages, but rather once the app refreshes.

--- src/screens/MainScreens/HomeScreen/HomeScreen.js
+++ src/screens/MainScreens/HomeScreen/HomeScreen.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import ResourcePage from '../../../components/ResourcePage';
 import { db } from '../../../firebase/firebase.js';
+
 export default function HomeScreen() {
-  var starCountRef = db.ref('inspirational_quotes/1');
-  starCountRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-  });
+  const [inspirationalQuote, setInspirationalQuote] = useState('');
+
+  function updateQuote(updatedQuote) {
+    setInspirationalQuote(updatedQuote);
+  }
+
+  const getInspirationalQuote = () => {
+    // get a random number
+    // add 1 because of the way the quotes are represented in realtime db -- should change that
+    // to start from 0
+    const randomNumber = Math.floor(Math.random() * 2) + 1; // replace 2 w total number of quotes
+
+    // read database inspirational quote
+    var quoteDbRef = db.ref('inspirational_quotes/' + randomNumber);
+
+    // extract data from db reference and update the current quote
+    quoteDbRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      updateQuote(data);
+    });
+  };
+
+  // the empty array means the function will only be called once
+  useEffect(() => {
+    getInspirationalQuote()
+  }, []);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Home Screen</Text>
+      <Text>{inspirationalQuote}</Text>
     </View>
   );
 }
