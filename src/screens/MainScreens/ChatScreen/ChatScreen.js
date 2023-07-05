a4de47b310530b2a6e5a1838618fda05b3632ce4
cyohn <cyohn@live.com>
2023-07-05 16:52:00 -0400

    Seperating the styles

--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -5,10 +5,17 @@ import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
 const ChatScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Chat Screen</Text>
+      <Text style={styles.text}>Chat Screen</Text>
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 16,
+        fontWeight:'bold',
+    }
+})
 
 export default ChatScreen;
