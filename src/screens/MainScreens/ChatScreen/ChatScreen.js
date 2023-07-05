985fd677b0bb116b0e07b2d38b70b62029752f3e
cyohn <cyohn@live.com>
2023-07-05 16:34:14 -0400

    First commit, added yarn.lock

--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -1,13 +1,14 @@
-import * as React from 'react';
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
+
 
 const ChatScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Chat Screen</Text>
 
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Chat Screen</Text>
-        </View>
-    )
-}
+    </View>
+  );
+};
 
 export default ChatScreen;
