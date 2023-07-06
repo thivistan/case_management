ae28f8f3da23a60a3015c81f23b968a86ce74444
cyohn <cyohn@live.com>
2023-07-06 16:45:07 -0400

    Saving a working state after removing errors

--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
 
 
+
 const ChatScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={styles.text}>Chat Screen</Text>
-
+      
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    text:{
-        fontSize: 16,
-        fontWeight:'bold',
-    }
-})
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default ChatScreen;
--- src/screens/MainScreens/GoalsScreen/GoalsScreen.js
+++ src/screens/MainScreens/GoalsScreen/GoalsScreen.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; //message1 and phone icon
+import { Entypo } from '@expo/vector-icons'; //edit pencil icon
 
 const GoalsScreen = () => {
-
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Goals Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Goals Screen</Text>
+    </View>
+  );
+};
 
 export default GoalsScreen;
