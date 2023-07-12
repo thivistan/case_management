990a12e2650187306813fa11f2b2c9e13b11ba5a
cyohn <cyohn@live.com>
2023-07-12 14:51:40 -0400

    The icons are in place for the contact card

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,24 +1,28 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-//import { AntDesign } from '@expo/vector-icons'; //message1 and phone icon
-//import { Entypo } from '@expo/vector-icons'; //edit pencil icon
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; //message1 and phone icon
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+//You can find the icon catalog at: https://icons.expo.fyi/
 
 //TODO: find out where the photo is coming from
 //TODO: find out about the availability - where does the data come from, how to show on app
 
+/**
+ * 
+ * @returns 
+ */
+//<Image styles={{resizeMode: 'contain', height: 100, width:100}} source={require('../assets/images/thaddeus_logo.jpg')} />
 export default function ContactPersonListItem() {
   return (
-    <View>
-      <Image style={styles.image} source={{ uri: 'srcassetsimages\thaddeus_globe_only.svg' }} />
-
-      <View>
-        <Text>Contact Name</Text>
-        <Text>Extra line of text</Text>
-      </View>
-
-      <View>
-        
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <FontAwesome name="user-circle" size={24} color="black" />
+      <Text>Contact Name</Text>
+      <Text>Extra line of text</Text>
+      <AntDesign name="message1" size={24} color="black" />
+      <AntDesign name="phone" size={24} color="black" />
+      <MaterialCommunityIcons name="pencil" size={24} color="black" />
     </View>
   );
 }
--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
-
+import ContactPersonListItem from '../../../components/ContactListItem';
 
 
 const ChatScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={styles.text}>Chat Screen</Text>
-      
+      <ContactPersonListItem/>
       
     </View>
   );
