6b1fcc2b3df92841e5f66c44307dda0ce229bf44
cyohn <cyohn@live.com>
2023-07-13 15:11:32 -0400

    The contact card layout is complete and rendering properly. Will fine tune the styling at a later time. I'm researching teams integration to cre ate the functionality

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -10,44 +10,47 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 //TODO: find out about the availability - where does the data come from, how to show on app
 
 /**
- * 
- * @returns 
+ *
+ * @returns
  */
 //<Image styles={styles.image} source={require('../assets/images/thaddeus_logo.jpg')} />
 export default function ContactPersonListItem() {
   return (
     <View style={styles.container}>
-        
       <View style={styles.container}>
         <FontAwesome name="user-circle" size={50} color="black" borderRadius={25} />
       </View>
 
-      <View >
-        <Text>Contact Name</Text>
+      <View>
+        <Text style={styles.headingText}>Contact Name</Text>
 
         <View>
-          <Text>Extra line of text</Text>
+          <Text style={styles.secondaryText}>Extra line of text</Text>
         </View>
-
       </View>
 
       <View style={styles.container}>
         <AntDesign name="message1" style={styles.icons} />
-        <AntDesign name="phone" style={styles.icons}/>
-        <MaterialCommunityIcons name="pencil" style={styles.icons}/>
+        <AntDesign name="phone" style={styles.icons} />
+        <MaterialCommunityIcons name="pencil" style={styles.icons} />
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flexDirection: 'row', padding: 10, },
-  text: {fontSize: 30, color: 'blue'},
+  container: { flexDirection: 'row', padding: 10, backgroundColor: 'white' },
+  headingText: { fontSize: 25, color: '#00BFFF' },
+  secondaryText: { fontSize: 15, color: '#333' },
   image: {
     height: 100,
     width: 100,
     borderRadius: 50,
   },
-  icons: {fontSize:30, color:"black", paddingLeft: 20},
+  icons: { fontSize: 30, color: 'black', paddingLeft: 20, color: '#00BFFF' },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+  },
 });
--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -5,10 +5,18 @@ import ContactPersonListItem from '../../../components/ContactListItem';
 
 const ChatScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: "flex-start" }}>
-      <Text style={styles.text}></Text>
-      <ContactPersonListItem/>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
       
+      <View style={styles.container}>
+        <Text style={styles.text}>Therapist</Text>
+      </View>
+      <ContactPersonListItem />
+
+      <View style={styles.container}>
+        <Text style={styles.text}>Case Manager</Text>
+      </View>
+      <ContactPersonListItem />
+    
     </View>
   );
 };
@@ -18,6 +26,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  container: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'flex-start',
+    padding: 10,
+  }
 });
 
 export default ChatScreen;
