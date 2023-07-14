65572d0b43ee842bdfa357eedb7ad4fe808d52da
cyohn <cyohn@live.com>
2023-07-14 14:17:29 -0400

    Added firestore to firebase.js

--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
 import ContactPersonListItem from '../../../components/ContactListItem';
 
-
 const ChatScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-      
       <View style={styles.container}>
         <Text style={styles.text}>Therapist</Text>
       </View>
@@ -16,7 +14,6 @@ const ChatScreen = () => {
         <Text style={styles.text}>Case Manager</Text>
       </View>
       <ContactPersonListItem />
-    
     </View>
   );
 };
@@ -31,7 +28,7 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'flex-start',
     padding: 10,
-  }
+  },
 });
 
 export default ChatScreen;
