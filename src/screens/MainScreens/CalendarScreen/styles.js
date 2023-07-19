4c338e5f8825c24c331855e2f26062615911ed56
riya-mathur <riyamathur898@gmail>
2023-07-19 12:07:24 -0700

    Rough layout of the add events screen

---
+++ src/screens/MainScreens/CalendarScreen/styles.js
@@ -0,0 +1,174 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    padding: 20,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    color: '#00BFFF',
+  },
+  addButton: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  eventKeysContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+  eventKey: {
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  eventKeyCircle: {
+    width: 25,
+    height: 25,
+    borderRadius: 25,
+  },
+  eventKeyLabel: {
+    color: 'white',
+    marginTop: 4,
+  },
+  calendarContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  calendarHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  calendarArrow: {
+    marginHorizontal: 10,
+  },
+  calendarMonth: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  calendarGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  calendarDate: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#00BFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginRight: 10,
+  },
+  calendarDateText: {
+    color: 'white',
+  },
+  weeklyRemindersContainer: {
+    marginBottom: 10,
+  },
+  weeklyRemindersText: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 10,
+  },
+  eventScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ADD8E6',
+    padding: 20,
+  },
+  eventScreenTitle: {
+    fontSize: 24,
+    color: 'white',
+    marginBottom: 20,
+  },
+  eventInput: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  boldPlaceholderText: {
+    fontWeight: 'bold',
+    color: 'lightblue', 
+  },
+  titleContainer: {
+    borderRadius: 10,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleRectangle: {
+    borderRadius: 10,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleInput: {
+    fontSize: 20,
+    color: 'lightblue',
+  },
+  spacing: {
+    marginBottom: 10,
+  },
+  keysSpacing: {
+    marginTop: 10,
+  },
+  notesContainer: {
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 8,
+  },
+  notesRectangle: {
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 8,
+  },
+  notesInput: {
+    fontSize: 20,
+    color: 'lightblue',
+  },
+  dateContainer: {
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 8,
+  },
+  dateRectangle: {
+    borderRadius: 7,
+    backgroundColor: 'white',
+    padding: 7,
+  },
+  dateInput: {
+    fontSize: 20,
+    color: 'lightblue',
+  },
+  repeatContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  repeatLabel: {
+    fontSize: 16,
+    color: 'white',
+  },
+  saveButton: {
+    backgroundColor: '#00BFFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  saveButtonText: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default styles;
