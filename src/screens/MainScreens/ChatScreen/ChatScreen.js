19afa4edf28363b2d76a6563df34bd2c4e543a3f
cyohn <cyohn@live.com>
2023-07-18 23:12:38 -0400

    Set up the useState for the floating action button

--- src/screens/MainScreens/ChatScreen/ChatScreen.js
+++ src/screens/MainScreens/ChatScreen/ChatScreen.js
@@ -1,33 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
 import ContactPersonListItem from '../../../components/ContactListItem';
 import FloatingActionAdd from '../../../components/FloatingActionAddBtn';
 import ContactSelectPopup from '../../PopupScreens/ContactSelectPopup';
 
 const ChatScreen = () => {
+  
+  const [buttonPopup, setButtonPopup] = useState(false);
+  
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
       <View style={styles.container}>
         <Text style={styles.text}>Therapist</Text>
       </View>
+
       <ContactPersonListItem />
 
       <View style={styles.container}>
         <Text style={styles.text}>Case Manager</Text>
       </View>
+      
       <ContactPersonListItem />
-      <FloatingActionAdd/>
+      
+      <FloatingActionAdd onClick={() => setButtonPopup(true)}/>
 
       
-<View>
-        <ContactSelectPopup trigger={true}></ContactSelectPopup>
-    </View>
+
 
     </View>
   
 
   );
-
+      <View>
+        <ContactSelectPopup trigger={buttonPopup}></ContactSelectPopup>
+      </View>
     
 
 
