26740f1837a0faf508be8bccf4b1599d29981c23
Shannon Lee <shan1020@rams.colostate.edu>
2023-08-01 16:41:44 -0600

    forms screen beginning design

--- src/screens/ServicesScreens/FormsServicesScreen/FormsServicesScreen.js
+++ src/screens/ServicesScreens/FormsServicesScreen/FormsServicesScreen.js
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
+import { BasicInfoButton } from '../../../components/ResourceButtons/FormButtons';
 
 const FormsServicesScreen = () => {
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.text}>Most Common</Text>
+      <View>
+        <Text style={styles.MostCommonText}>Most Common</Text>
+        <BasicInfoButton title="Basic Information" />
+      </View>
+
+      <View>
+        <Text style={styles.SupportGroupText}>Support Groups</Text>
+      </View>
     </ScrollView>
   );
 };
@@ -17,12 +25,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ccf3fb',
   },
-  text: {
+  MostCommonText: {
     color: '#31c5e2',
     fontWeight: 'bold',
     fontSize: '20',
     position: 'absolute',
     left: 20,
     top: 65,
+  },
+  SupportGroupText: {
+    color: '#31c5e2',
+    fontWeight: 'bold',
+    fontSize: '20',
+    // position: 'absolute',
+    // left: 20,
+    // top: 180,
   },
 });
