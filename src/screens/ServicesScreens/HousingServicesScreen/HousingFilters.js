c420174a4a2b6b1c295b6a3cdbff68985fbf16d4
Jesseh527 <jesseh52700@gmail.com>
2023-08-26 14:50:45 -0700

    fixed check boxes

--- src/screens/ServicesScreens/HousingServicesScreen/HousingFilters.js
+++ src/screens/ServicesScreens/HousingServicesScreen/HousingFilters.js
@@ -1,19 +1,43 @@
 import { StyleSheet, Text, View, TextInput, Modal, TouchableOpacity } from 'react-native';
 // import CheckBox from '@react-native-community/checkbox';
 import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HousingFilters = ({ visible }) => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  // const [homelessChecked, setHomelessChecked] = useState(false);
-  // const [seniorsChecked, setSeniorsChecked] = useState(false);
-  // const [familyChildrenChecked, setFamilyChildrenChecked] = useState(false);
+  const [homelessChecked, setHomelessChecked] = useState(false);
+  const [seniorsChecked, setSeniorsChecked] = useState(false);
+  const [familyChildrenChecked, setFamilyChildrenChecked] = useState(false);
+  const [bubbleCheck, SetBubbleCheck] = useState(25);
   const options = ['Option 1', 'Option 2', 'Option 3'];
+  const [Gender, SetGender] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   };
+  function CustomCheckBox() {
+    const [isChecked, setIsChecked] = useState(false);
 
+    const toggleCheckBox = () => {
+      setIsChecked(!isChecked);
+    };
+
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={toggleCheckBox}
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Icon
+            name={isChecked ? 'check-square-o' : 'square-o'}
+            size={24}
+            color={isChecked ? 'green' : '#47d7f2'}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   const selectOption = (option) => {
     setSelectedOption(option);
     toggleDropdown();
@@ -39,19 +63,58 @@ const HousingFilters = ({ visible }) => {
       </View>
       <Text style={styles.textStyle}>DISTANCE</Text>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <TouchableOpacity style={styles.bubbleStyle}></TouchableOpacity>
-        <TouchableOpacity style={styles.bubbleStyle}></TouchableOpacity>
-        <TouchableOpacity style={styles.bubbleStyle}></TouchableOpacity>
-        <TouchableOpacity style={styles.bubbleStyle}></TouchableOpacity>
-        <TouchableOpacity style={styles.bubbleStyle}></TouchableOpacity>
+        <View>
+          <TouchableOpacity
+            style={[
+              styles.bubbleStyle,
+              { backgroundColor: bubbleCheck == 5 ? '#47d7f2' : 'white' },
+            ]}
+            onPress={() => SetBubbleCheck(5)}
+          ></TouchableOpacity>
+          <Text style={[styles.buttonText, { paddingTop: 4 }]}>5</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[
+              styles.bubbleStyle,
+              { backgroundColor: bubbleCheck == 10 ? '#47d7f2' : 'white' },
+            ]}
+            onPress={() => SetBubbleCheck(10)}
+          ></TouchableOpacity>
+          <Text style={[styles.buttonText, { paddingTop: 4 }]}>10</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[
+              styles.bubbleStyle,
+              { backgroundColor: bubbleCheck == 15 ? '#47d7f2' : 'white' },
+            ]}
+            onPress={() => SetBubbleCheck(15)}
+          ></TouchableOpacity>
+          <Text style={[styles.buttonText, { paddingTop: 4 }]}>15</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[
+              styles.bubbleStyle,
+              { backgroundColor: bubbleCheck == 20 ? '#47d7f2' : 'white' },
+            ]}
+            onPress={() => SetBubbleCheck(20)}
+          ></TouchableOpacity>
+          <Text style={[styles.buttonText, { paddingTop: 4 }]}>20</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[
+              styles.bubbleStyle,
+              { backgroundColor: bubbleCheck == 25 ? '#47d7f2' : 'white' },
+            ]}
+            onPress={() => SetBubbleCheck(25)}
+          ></TouchableOpacity>
+          <Text style={[styles.buttonText, { paddingTop: 4 }]}>25</Text>
+        </View>
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={styles.buttonText}>5</Text>
-        <Text style={styles.buttonText}>10</Text>
-        <Text style={styles.buttonText}>15</Text>
-        <Text style={styles.buttonText}>20</Text>
-        <Text style={styles.buttonText}>25</Text>
-      </View>
+
       <View style={styles.textInputContainer}>
         <TextInput
           style={styles.textInput}
@@ -77,29 +140,68 @@ const HousingFilters = ({ visible }) => {
         </View>
       </Modal>
       <Text style={styles.textStyle}>WHO</Text>
-      {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <View></View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <View style={styles.checkboxContainer}>
-          <CheckBox
+          <CustomCheckBox
             value={homelessChecked}
             onValueChange={() => setHomelessChecked(!homelessChecked)}
           />
-          <Text style={styles.checkboxLabel}>Homeless</Text>
+          <Text style={styles.checkboxLabel}>Homeless </Text>
         </View>
         <View style={styles.checkboxContainer}>
-          <CheckBox
+          <CustomCheckBox
             value={seniorsChecked}
             onValueChange={() => setSeniorsChecked(!seniorsChecked)}
           />
-          <Text style={styles.checkboxLabel}>Seniors</Text>
+          <Text style={styles.checkboxLabel}>Seniors </Text>
         </View>
         <View style={styles.checkboxContainer}>
-          <CheckBox
+          <CustomCheckBox
             value={familyChildrenChecked}
             onValueChange={() => setFamilyChildrenChecked(!familyChildrenChecked)}
           />
-          <Text style={styles.checkboxLabel}>Family and Children</Text>
+          <Text style={styles.checkboxLabel}>Family and Children </Text>
         </View>
-      </View> */}
+      </View>
+      <Text style={styles.textStyle}>GENDER</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableOpacity
+          onPress={() => SetGender('MALE')}
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Icon
+            name={Gender == 'MALE' ? 'check-square-o' : 'square-o'}
+            size={24}
+            color={Gender == 'MALE' ? 'green' : '#47d7f2'}
+          />
+          <Text>MALE</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => SetGender('FEMALE')}
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Icon
+            name={Gender == 'FEMALE' ? 'check-square-o' : 'square-o'}
+            size={24}
+            color={Gender == 'FEMALE' ? 'green' : '#47d7f2'}
+          />
+          <Text>FEMALE</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => SetGender('TRANS')}
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Icon
+            name={Gender == 'TRANS' ? 'check-square-o' : 'square-o'}
+            size={24}
+            color={Gender == 'TRANS' ? 'green' : '#47d7f2'}
+          />
+          <Text>TRANS</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
