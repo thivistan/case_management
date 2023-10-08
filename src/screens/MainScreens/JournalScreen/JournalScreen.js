36e0ba746a22986c0c94587d811bd813ee23e475
Dennis Lustre <dennis.lustre@gmail.com>
2023-10-08 16:18:56 -0700

    Revert "Implement expo-camera and basic camera usage"
    This reverts commit da413c332786f2c4e1fd2fe082dc4b4ebbf638b1.

--- src/screens/MainScreens/JournalScreen/JournalScreen.js
+++ src/screens/MainScreens/JournalScreen/JournalScreen.js
@@ -1,123 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
-import { Camera } from 'expo-camera';
-import { createAssetAsync, createAlbumAsync, requestPermissionsAsync, getAlbumAsync, addAssetsToAlbumAsync } from 'expo-media-library';
+import * as React from 'react';
+import { View, Text } from "react-native";
 
-export default function JournalScreen() {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [cameraRef, setCameraRef] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back);
-
-    useEffect(() => {
-        (async () => {
-            const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
-            const { status: mediaLibStatus } = await requestPermissionsAsync();
-
-            setHasPermission(cameraStatus === 'granted' && mediaLibStatus === 'granted');
-        })();
-    }, []);
-
-    async function takePicture() {
-        if (cameraRef) {
-            const photo = await cameraRef.takePictureAsync();
-            const asset = await createAssetAsync(photo.uri);
-            const album = await getAlbumAsync('Thaddeus');
-
-            if (album === null) {
-                await createAlbumAsync('Thaddeus', asset, false);
-            } else {
-                await addAssetsToAlbumAsync([asset], album, false);
-            }
-        }
-    };
-
-    const [isRecording, setIsRecording] = useState(false);
-
-    async function recordVideo() {
-        if (!cameraRef) return;
-
-        if (isRecording) {
-            setIsRecording(false);
-            cameraRef.stopRecording();
-            console.log("Stopped recording");
-        } else {
-            setIsRecording(true);
-            try {
-                const video = await cameraRef.recordAsync({ quality: '720p' });
-                console.log("Finished recording");
-
-                const asset = await createAssetAsync(video.uri);
-                const album = await getAlbumAsync('Thaddeus');
-
-                if (album === null) {
-                    await createAlbumAsync('Thaddeus', asset, false);
-                    console.log("Created new album");
-                } else {
-                    await addAssetsToAlbumAsync([asset], album, false);
-                    console.log("Added video to album");
-                }
-            } catch (error) {
-                console.error("Error recording video:", error);
-            } finally {
-                setIsRecording(false);
-            }
-        }
-    }
-
-    if (hasPermission === null) {
-        return <View />;
-    }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-    }
+const JournalScreen = () => {
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Camera style={{ flex: 1 }} type={type} ref={(ref) => setCameraRef(ref)}>
-                <View
-                    style={{
-                        flex: 1,
-                        backgroundColor: 'transparent',
-                        flexDirection: 'row',
-                    }}>
-                    <TouchableOpacity
-                        style={{
-                            flex: 0.1,
-                            alignSelf: 'flex-end',
-                            alignItems: 'center',
-                        }}
-                        onPress={() => {
-                            setType(
-                                type === Camera.Constants.Type.back
-                                    ? Camera.Constants.Type.front
-                                    : Camera.Constants.Type.back
-                            );
-                        }}>
-                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{
-                            flex: 0.1,
-                            alignSelf: 'flex-end',
-                            alignItems: 'center',
-                        }}
-                        onPress={takePicture}>
-                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Snap </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={{
-                            flex: 0.1,
-                            alignSelf: 'flex-end',
-                            alignItems: 'center',
-                        }}
-                        onPress={recordVideo}>
-                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                            {isRecording ? "Stop" : "Record"}
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </Camera>
-        </SafeAreaView>
-    );
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Journal Screen</Text>
+        </View>
+    )
 }
+
+export default JournalScreen;
