8d78c6ac39b4a03b2d236155a77e0a5b23cf4928
Dennis Lustre <dennis.lustre@gmail.com>
2023-10-15 21:01:12 -0700

    Remove old service screens

--- src/screens/ServicesScreens/EmploymentServicesScreen/EmploymentServicesScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ServiceScreen from '../../../components/ServiceScreen';
-import categoryData from '../../../data/categories/employment.json';
-
-export default function EmploymentServicesScreen({ navigation }) {
-  return <ServiceScreen navigation={navigation} categoryData={categoryData} />
-}
