52732fa4f7dcfac66621de39f867793e28b75d73
Noah Fajarda <noahfajarda@gmail.com>
2023-10-17 20:58:53 -0700

    adjusted spacing

--- src/screens/MainScreens/CalendarScreen/CalendarRepo.js
+++ src/screens/MainScreens/CalendarScreen/CalendarRepo.js
@@ -13,90 +13,90 @@ const renderEventScreen = () => {
   const [selectedDate, setSelectedDate] = useState('');
   const [repeatWeekly, setRepeatWeekly] = useState(false);
 
-    const handleSaveEvent = () => {
-        // Logic for saving the event
-        setShowEventScreen(false);
-      };
+  const handleSaveEvent = () => {
+    // Logic for saving the event
+    setShowEventScreen(false);
+  };
 
-    const renderEventKey = (label, color) => (
-        <View style={styles.eventKey}>
-          <View style={[styles.eventKeyCircle, { backgroundColor: color }]} />
-          <Text style={styles.eventKeyLabel}>{label}</Text>
-        </View>
-      );
+  const renderEventKey = (label, color) => (
+    <View style={styles.eventKey}>
+      <View style={[styles.eventKeyCircle, { backgroundColor: color }]} />
+      <Text style={styles.eventKeyLabel}>{label}</Text>
+    </View>
+  );
 
-    return (
+  return (
 
-      <View style={styles.eventScreen}>
+    <View style={styles.eventScreen}>
 
-        {/* Page Title */}
-        <Text style={styles.eventScreenTitle}>New Event</Text>
+      {/* Page Title */}
+      <Text style={styles.eventScreenTitle}>New Event</Text>
 
-        {/* Title */}
-        <View style={[styles.eventInput, styles.titleContainer]}>
-          <View style={styles.titleRectangle}>
-            <TextInput
-              style={[styles.titleInput, styles.boldPlaceholderText]}
-              placeholder="Title"
-              onChangeText={setTitle}
-              value={title}
-            />
-          </View>
-        </View>
-
-        {/* Spacing */}
-        <View style={styles.spacing} />
-
-        {/* Event Keys */}
-        <View style={[styles.eventKeysContainer, styles.keysSpacing]}>
-          {renderEventKey('CM Appt', 'green')}
-          {renderEventKey('Therapy', 'yellow')}
-          {renderEventKey('Chat', 'red')}
-          {renderEventKey('Tasks', 'orange')}
-          {renderEventKey('Support Group', 'purple')}
-        </View>
-
-        {/* Notes */}
-        <View style={[styles.eventInput, styles.notesContainer]}>
-          <View style={styles.notesRectangle}>
-            <TextInput
-              style={[styles.notesInput, styles.boldPlaceholderText]}
-              placeholder="Notes"
-              multiline={true}
-              onChangeText={setNotes}
-              value={notes}
-            />
-          </View>
-        </View>
-
-        {/* Date */}
-        <View style={[styles.eventInput, styles.dateContainer]}>
-          <View style={styles.dateRectangle}>
-            <TextInput
-              style={[styles.dateInput, styles.boldPlaceholderText]}
-              placeholder="Date"
-              onChangeText={setSelectedDate}
-              value={selectedDate}
-            />
-          </View>
-        </View>
-
-        {/* Repeat */}
-        <View style={[styles.eventInput, styles.repeatContainer]}>
-          <Text style={[styles.repeatLabel, { color: '#00BFFF', fontSize: 18 }]}>Repeat:</Text>
-          <Switch
-            trackColor={{ false: 'gray', true: 'green' }}
-            thumbColor={repeatWeekly ? 'white' : 'gray'}
-            value={repeatWeekly}
-            onValueChange={setRepeatWeekly}
+      {/* Title */}
+      <View style={[styles.eventInput, styles.titleContainer]}>
+        <View style={styles.titleRectangle}>
+          <TextInput
+            style={[styles.titleInput, styles.boldPlaceholderText]}
+            placeholder="Title"
+            onChangeText={setTitle}
+            value={title}
           />
         </View>
-
-        {/* Save Button */}
-        <TouchableOpacity style={styles.saveButton} onPress={handleSaveEvent}>
-          <Text style={styles.saveButtonText}>Create</Text>
-        </TouchableOpacity>
       </View>
+
+      {/* Spacing */}
+      <View style={styles.spacing} />
+
+      {/* Event Keys */}
+      <View style={[styles.eventKeysContainer, styles.keysSpacing]}>
+        {renderEventKey('CM Appt', 'green')}
+        {renderEventKey('Therapy', 'yellow')}
+        {renderEventKey('Chat', 'red')}
+        {renderEventKey('Tasks', 'orange')}
+        {renderEventKey('Support Group', 'purple')}
+      </View>
+
+      {/* Notes */}
+      <View style={[styles.eventInput, styles.notesContainer]}>
+        <View style={styles.notesRectangle}>
+          <TextInput
+            style={[styles.notesInput, styles.boldPlaceholderText]}
+            placeholder="Notes"
+            multiline={true}
+            onChangeText={setNotes}
+            value={notes}
+          />
+        </View>
+      </View>
+
+      {/* Date */}
+      <View style={[styles.eventInput, styles.dateContainer]}>
+        <View style={styles.dateRectangle}>
+          <TextInput
+            style={[styles.dateInput, styles.boldPlaceholderText]}
+            placeholder="Date"
+            onChangeText={setSelectedDate}
+            value={selectedDate}
+          />
+        </View>
+      </View>
+
+      {/* Repeat */}
+      <View style={[styles.eventInput, styles.repeatContainer]}>
+        <Text style={[styles.repeatLabel, { color: '#00BFFF', fontSize: 18 }]}>Repeat:</Text>
+        <Switch
+          trackColor={{ false: 'gray', true: 'green' }}
+          thumbColor={repeatWeekly ? 'white' : 'gray'}
+          value={repeatWeekly}
+          onValueChange={setRepeatWeekly}
+        />
+      </View>
+
+      {/* Save Button */}
+      <TouchableOpacity style={styles.saveButton} onPress={handleSaveEvent}>
+        <Text style={styles.saveButtonText}>Create</Text>
+      </TouchableOpacity>
+    </View>
   );
 
 };
--- src/screens/MainScreens/CalendarScreen/MonthlyCalendar.js
+++ src/screens/MainScreens/CalendarScreen/MonthlyCalendar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, TextInput, } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import styles from './styles';
@@ -10,89 +10,89 @@ import renderEventScreen from './CalendarRepo';
 
 const MonthlyCalendar = () => {
   const Spacer = ({ size }) => <View style={{ width: size, height: size }} />;
-  const [SelectedDate, setSelecteddate] = useState(false);    
+  const [SelectedDate, setSelecteddate] = useState(false);
   const navigation = useNavigation();
-    const handleDayPress = (day) =>    {
-        setSelecteddate(day.dateString);    
-    };
+  const handleDayPress = (day) => {
+    setSelecteddate(day.dateString);
+  };
 
-    const renderEventKey = (label, color) => (
-        <View style={styles.eventKey}>
-          <View style={[styles.eventKeyCircle, { backgroundColor: color }]} />
-          <Text style={styles.eventKeyLabel}>{label}</Text>
+  const renderEventKey = (label, color) => (
+    <View style={styles.eventKey}>
+      <View style={[styles.eventKeyCircle, { backgroundColor: color }]} />
+      <Text style={styles.eventKeyLabel}>{label}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles2.container}>
+
+      <View style={styles.eventKeysContainer}>
+
+        {renderEventKey('CM Appt', 'green')}
+        {renderEventKey('Therapy', 'yellow')}
+        {renderEventKey('Chat', 'red')}
+        {renderEventKey('Tasks', 'orange')}
+        {renderEventKey('Support Group', 'purple')}
+      </View>
+
+      <Text style={styles2.selectedDate}>Monthly Calendar</Text>
+      <Spacer size={30} />
+
+      <Calendar
+        style={styles2.calendar}
+        contentContainerStyle={styles2.calendarContainer}
+
+        markedDates={{
+          [SelectedDate]: {
+            selected: true,
+            selectedColor: 'blue',
+          },
+        }}
+
+        onDayPress={handleDayPress}
+      // onDayLongPress= {handleDayPress2(SelectedDate)}
+
+      />
+
+      {SelectedDate && (
+        <View style={styles2.eventContainer}>
+          <Text style={styles2.selectedDate}>{SelectedDate}</Text>
+          {/* Add event creation form or any other desired functionality */}
+
         </View>
-      );
-     
-    return (       
-        <View style={styles2.container}>
+      )}
+    </View>
+  );
+};
 
-            <View style={styles.eventKeysContainer}>
-              
-            {renderEventKey('CM Appt', 'green')}
-            {renderEventKey('Therapy', 'yellow')}
-            {renderEventKey('Chat', 'red')}
-            {renderEventKey('Tasks', 'orange')}
-            {renderEventKey('Support Group', 'purple')}
-            </View>
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
 
-          <Text style={styles2.selectedDate}>Monthly Calendar</Text>
-          <Spacer size={30} />
-          
-            <Calendar
-            style={styles2.calendar}
-            contentContainerStyle={styles2.calendarContainer}
-                    
-              markedDates={{
-                [SelectedDate]: {
-                  selected: true,
-                  selectedColor: 'blue',
-                },
-              }}
+  },
 
-              onDayPress= {handleDayPress}
-              // onDayLongPress= {handleDayPress2(SelectedDate)}
-                           
-            />
-            
-          {SelectedDate && (
-            <View style={styles2.eventContainer}>
-              <Text style={styles2.selectedDate}>{SelectedDate}</Text>
-              {/* Add event creation form or any other desired functionality */}
-                         
-            </View>
-          )}
-        </View>
-      );
-    };
-    
-    const styles2 = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-      },
+  calendarContainer: {
+    flexWrap: 10,
 
-      calendarContainer: {
-        flexWrap: 10,
-       
-      },
+  },
 
-      calendar: {
-        aspectRatio: 0.9,
-        resizeMode: 'contain'
-      },
+  calendar: {
+    aspectRatio: 0.9,
+    resizeMode: 'contain'
+  },
 
-      eventContainer: {
-        marginTop: 20,
-      },
+  eventContainer: {
+    marginTop: 20,
+  },
 
-      selectedDate: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'white',
-       
-      },
-    });
-    
-    export default MonthlyCalendar;
+  selectedDate: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+
+  },
+});
+
+export default MonthlyCalendar;
